91e594b40cedf6ec8554e9248a129b86964befc2
Dong Pei <peidong2009@gmail.com>
2017-06-05 14:39:40 -0500

    finished week5 part1

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -4,14 +4,17 @@ import java.util.TreeSet;
 
 /**
  * @author UC San Diego Intermediate MOOC team
- *
+ * @modified by Dong Pei
+ * @modified on June 2017
  */
 public class DictionaryBST implements Dictionary 
 {
-   private TreeSet<String> dict;
+	private TreeSet<String> dict;
 	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
+	public DictionaryBST()
+	{
+		dict = new TreeSet<String>();
+	}
 	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -19,23 +22,27 @@ public class DictionaryBST implements Dictionary
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
-    public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+    public boolean addWord(String word) 
+    {
+        String lowerWord = word.toLowerCase();    	
+    	if (isWord(lowerWord)){
+    		return false;
+    	} else {
+    		dict.add(lowerWord);
+            return true;
+    	}
     }
 
 
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+        return dict.size(); 
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+    	String lowerS = s.toLowerCase();
+    	return dict.contains(lowerS);
     }
-
 }
--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -13,33 +13,43 @@ public class DictionaryLL implements Dictionary
 
 	private LinkedList<String> dict;
 	
-    // TODO: Add a constructor
-	
-
+    
+	public DictionaryLL()
+	{
+		dict = new LinkedList<String>();
+	}
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	String lowerWord = word.toLowerCase();
-    	
-        return true;
+    	String lowerWord = word.toLowerCase();    	
+    	if (isWord(lowerWord)){
+    		return false;
+    	} else {
+    		dict.add(lowerWord);
+            return true;
+    	}
     }
 
 
     /** Return the number of words in the dictionary */
     public int size()
     {
-        // TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-        //TODO: Implement this method
+    	String lowerS = s.toLowerCase();
+    	for (String word : dict){
+        	if (word.equals(lowerS)){
+        		return true;
+        	}
+        }
         return false;
     }
-
+    
     
 }
